45cee1edc63d1931d21ae11cc51c83cb32ccfec7
Taylor Burkhalter <96509561+taylorburk@users.noreply.github.com>
2023-06-11 23:18:46 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,53 @@
+from flask import Flask, request, render_template, jsonify, session
+from boggle import Boggle
+from flask import session, make_response
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "fdfgkjtjkkg45yfdb"
+
+boggle_game = Boggle()
+@app.route("/")
+def homepage():
+    """Show board."""
+
+    board = boggle_game.make_board()
+    session['board'] = board
+    highscore = session.get("highscore", 0)
+    nplays = session.get("nplays", 0)
+
+    return render_template("index.html", board=board,
+                           highscore=highscore,
+                           nplays=nplays)
+
+
+@app.route("/check-word")
+def check_word():
+    """Check if word is in dictionary."""
+
+    word = request.args["word"]
+    board = session["board"]
+    response = boggle_game.check_valid_word(board, word)
+
+    return jsonify({'result': response})
+
+
+@app.route("/post-score", methods=["POST"])
+def post_score():
+    """Receive score, update nplays, update high score if appropriate."""
+
+    score = request.json["score"]
+    highscore = session.get("highscore", 0)
+    nplays = session.get("nplays", 0)
+
+    session['nplays'] = nplays + 1
+    session['highscore'] = max(score, highscore)
+
+    return jsonify(brokeRecord=score > highscore)
+
+@app.route("/test", methods=["GET"])
+def test_function():
+    template = render_template("new.html", test_variable='testing', another_variable = ['value 1', 'value 2', 'value 3'])
+    response = make_response(template)
+    response.set_cookie("test_cookie2", 'test cookie')
+    session['test_session']='session'
+    return response
